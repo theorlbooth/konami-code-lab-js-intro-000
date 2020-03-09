055cc951af6ad578a7bb1255871f368f95887855
Theo Booth <theorlbooth@googlemail.com>
2020-03-09 16:21:42 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,19 @@ const codes = [
   "a"
 ];
 
+var key_log = 0
+
 function init() {
-  // your code here
+document.body.addEventListener("keydown",function(e) {
+  var key = e.key
+  if(key === codes[key_log]) {
+    key_log += 1
+    if (key_log === codes.length) {
+      alert("Well done, you have found the Easter Egg!!")
+      key_log = 0
+    }
+  } else {
+    key_log = 0
+  }
+})
 }
